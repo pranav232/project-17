1938d7ae7294ab690921a8eb0b7ee3b53297b8b2
pranav232 <73473120+pranav232@users.noreply.github.com>
2020-12-15 17:00:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+
+var monkey , monkey_running
+var banana ,bananaImage
+var FoodGroup
+var score,obstacle,obstacleGroup,obstacleimage
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+ //bgimage=loadImage("jungle.jpg");
+  obstacleimage=loadImage("obstacle.png");
+}
+
+
+
+
+function setup() {
+  createCanvas(600,600)
+  
+ /*bground=createSprite(200,200,400,400);
+  bground.shapeColor="cyan";
+  bground.velocityX=3;
+  bground.addImage("jungle",bgimage);*/
+  
+  
+monkey=createSprite(80,400,20,20);
+monkey.addAnimation("moving",monkey_running);
+monkey.scale=0.05; 
+  
+ground=createSprite(400,420,900,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  console.log(ground.x);
+  ground.visible=false;
+  
+  FoodGroup=createGroup();
+  obstaclegroup=createGroup();
+  score=0;
+}
+
+
+function draw() {
+background("white");
+  textSize(20);
+  stroke("green");
+  fill("green");
+  
+ /* if(bground.x<400){
+    bground.x=200;
+    bground.y=200;
+bground.velocityX=3;
+  }*/
+  if(keyDown("space")){
+    monkey.velocityY=-12;
+    }
+  if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  monkey.velocityY = monkey.velocityY + 0.8;
+  monkey.collide(ground);
+  if (FoodGroup.isTouching(monkey)){
+    score=score+2;
+    FoodGroup.destroyEach();
+    monkey.scale=monkey.scale+0.02;
+  }
+  obstacles();
+ 
+  food();
+  //obstacles();
+drawSprites(); 
+  text("SCORE : "+score,50,50);
+}
+function obstacles(){
+  if(frameCount%100===0){
+    obstacle=createSprite(600,400,10,10);
+    obstacle.addImage(obstacleimage);   
+    obstacle.scale=0.1
+    obstacle.velocityX=-2 
+    obstaclegroup.add(obstacle);
+  }
+}
+function food(){
+  if(frameCount%80===0)
+{
+ var banana=createSprite(500,165,10,40); 
+   banana.y= Math.round(random(120,200));
+  banana.velocityX = -3;
+  banana.addImage(bananaImage);
+  banana.scale=0.1;
+  banana.lifetime=200;
+  
+  FoodGroup.add(banana);
+}
+}
